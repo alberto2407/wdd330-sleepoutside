4872a2e753f6c7416e1e34308b200d87d96d4ae3
Cherry-Machado <cherry5467@gmail.com>
2025-09-20 02:27:43 -0300

    Total$ in cart individual task acivity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,12 +1,49 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, priceTotal } from "./utils.mjs";
+function deleteCartContent(event) {
+  const itemId = event.target.getAttribute("data-id");
+  let cartItems = getLocalStorage("so-cart");
+
+  const itemIndex = cartItems.findIndex((item) => item.Id === itemId);
+  cartItems[itemIndex].quantity -= 1;
+
+  if (cartItems[itemIndex].quantity <= 0) {
+    cartItems = cartItems.filter((item) => item.Id !== itemId);
+  }
+
+  setLocalStorage("so-cart", cartItems);
+  renderCartContents();
+}
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  const cartFooter = document.querySelector(".cart-footer");
+  if (cartItems.length > 0) {
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    const total = priceTotal(
+      cartItems,
+      (item) => item.FinalPrice * item.quantity,
+    );
+    document.querySelector(
+      ".cart-total__amount",
+    ).textContent = `$${total.toFixed(2)}`;
+    cartFooter.classList.remove("hide");
+    document.querySelectorAll(".cart-card__delete").forEach((button) => {
+      button.addEventListener("click", deleteCartContent);
+    });
+    document
+      .querySelector("button.checkout")
+      .addEventListener("click", () =>
+        window.location.replace("/checkout/index.html"),
+      );
+  } else {
+    document.querySelector(".product-list").innerHTML = "";
+    cartFooter.classList.add("hide");
+  }
 }
 
 function cartItemTemplate(item) {
+  const subtotal = (item.quantity * item.FinalPrice).toFixed(2);
   const newItem = `<li class="cart-card divider">
   <a href="#" class="cart-card__image">
     <img
@@ -20,6 +57,8 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: ${item.quantity}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="cart-card__delete" data-id=${item.Id}>X</span>
+  <p class="cart-card__subtotal">Subtotal: $${subtotal}</p>
 </li>`;
 
   return newItem;
